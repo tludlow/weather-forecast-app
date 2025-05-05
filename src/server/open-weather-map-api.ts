8b7d23416bc3d9cbdc4bd8bf6fa523aa8a906f8a
tludlow <twjludlow@gmail.com>
2025-05-05 19:38:33 +0100

    Get current weather from open weather map api

--- src/server/open-weather-map-api.ts
+++ src/server/open-weather-map-api.ts
@@ -30,3 +30,63 @@ export async function getGeocoding(input: { location: string }) {
 
   return location;
 }
+
+const OpenWeatherForecastWeatherSchema = z.object({
+  id: z.number(),
+  main: z.string(),
+  description: z.string(),
+  icon: z.string(),
+});
+
+// Only including the fields we care about for the point of this "demo" application, for brevity reasons
+const OpenWeatherMapCurrentWeatherApiResponseSchema = z.object({
+  dt: z.number(),
+  weather: OpenWeatherForecastWeatherSchema.array(),
+  main: z.object({
+    temp: z.number(),
+    humidity: z.number(),
+  }),
+  wind: z.object({ speed: z.number(), deg: z.number() }),
+});
+
+export const CurrentWeatherResponseSchema =
+  OpenWeatherMapCurrentWeatherApiResponseSchema.extend({
+    weather: OpenWeatherForecastWeatherSchema,
+  });
+
+export async function getCurrentWeather(input: { lat: number; lon: number }) {
+  if (input.lat < -90 || input.lat > 90) {
+    throw new Error(
+      'Invalid "lat" value, lat must be between -90 and 90 inclusive'
+    );
+  }
+
+  if (input.lon < -180 || input.lon > 180) {
+    throw new Error(
+      'Invalid "lon" value, lon must be between -180 and 180 inclusive'
+    );
+  }
+
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${input.lat}&lon=${input.lon}&units=metric&appid=${process.env.OPENWEATHERMAP_API_KEY}`
+  );
+  const responseJson = await response.json();
+  const data =
+    OpenWeatherMapCurrentWeatherApiResponseSchema.parse(responseJson);
+
+  // The "weather" response is an array here, but we only expect a singular element for this
+  // as it's the current weather forecast. Let's take that array and only return the single
+  // element so the consumer of this function has an easier time
+
+  const currentWeather = data.weather[0];
+  if (!currentWeather) {
+    throw new Error('Could not find the current weather information');
+  }
+
+  const finalData: z.infer<typeof CurrentWeatherResponseSchema> = {
+    ...data,
+    weather: currentWeather,
+  };
+
+  return finalData;
+}
