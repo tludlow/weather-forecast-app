cacd52ba32330ecc346388bd4a0e0bdcfb8375bc
tludlow <twjludlow@gmail.com>
2025-05-05 20:16:30 +0100

    5 day forecast toggle button

--- src/app/forecast/page.tsx
+++ src/app/forecast/page.tsx
@@ -1,8 +1,9 @@
+import Link from 'next/link';
 import { redirect } from 'next/navigation';
 import ForecastDetails from '~/components/forecast-details';
 
 interface ForecastPageProps {
-  searchParams: Promise<{ location?: string }>;
+  searchParams: Promise<{ location?: string; days?: string }>;
 }
 
 export async function generateMetadata(props: ForecastPageProps) {
@@ -16,10 +17,12 @@ export async function generateMetadata(props: ForecastPageProps) {
 export default async function ForecastPage(props: ForecastPageProps) {
   const searchParams = await props.searchParams;
 
-  const { location } = searchParams;
+  const { location, days } = searchParams;
   if (!location) {
     redirect('/');
   }
+
+  const forecastDays = Number(days ?? 3);
 
   const currentWeatherResponse = await fetch(
     `http://localhost:3000/api/weather?location=${location}`
@@ -42,6 +45,23 @@ export default async function ForecastPage(props: ForecastPageProps) {
       <div className='mt-6 flex flex-col items-center gap-y-3'>
         <p className='text-xl font-semibold'>Current forecast:</p>
         <ForecastDetails forecast={currentWeatherJson.weather} />
+      </div>
+
+      <div className='mt-12 border-t border-neutral-300 pt-4'>
+        <div className='flex items-center gap-x-4'>
+          <h2 className='text-xl font-bold'>{forecastDays} Day Forecast</h2>
+
+          {forecastDays !== 5 && (
+            <Link
+              href={`/forecast?location=${location}&days=5`}
+              className='text-blue-500 hover:underline'
+            >
+              See 5-day forecast?
+            </Link>
+          )}
+        </div>
+
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-4'></div>
       </div>
     </div>
   );
